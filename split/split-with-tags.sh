957844e11f479d025e27f3c096b2741668d833ab
dreamsbond <info@dreamsbond.com>
2020-03-04 01:07:02 +0800

    update.

--- split/split-with-tags.sh
+++ split/split-with-tags.sh
@@ -8,21 +8,19 @@ source "${CURRENT_DIR}/components.sh"
 
 rm -rf .subsplit/ && git subsplit init git@github.com:orzford/limoncello.git
 
-for component in "${COMPONENTS[@]}"
-do
-  ((THREAD_ID++))
-  THREAD_FOLDER=${TMP_DIR}/${THREAD_ID}
-  (\
-    mkdir ${THREAD_FOLDER} &&\
-    cp -r ${CURRENT_DIR}/.subsplit/ ${THREAD_FOLDER} &&\
-    cd ${THREAD_FOLDER} &&\
-    git subsplit publish --heads="${BRANCHES}" "${component}" -q \
-  ) &
+for component in "${COMPONENTS[@]}"; do
+    ((THREAD_ID++))
+    THREAD_FOLDER=${TMP_DIR}/${THREAD_ID}
+    (
+        mkdir ${THREAD_FOLDER} &&
+            cp -r ${CURRENT_DIR}/.subsplit/ ${THREAD_FOLDER} &&
+            cd ${THREAD_FOLDER} &&
+            git subsplit publish --heads="${BRANCHES}" "${component}" -q
+    ) &
 done
 
-for job in `jobs -p`
-do
-  wait $job
+for job in $(jobs -p); do
+    wait $job
 done
 
 rm -rf .subsplit/ && rm -rf ${TMP_DIR}
