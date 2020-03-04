9c002b6feb58f380003bf3a33363a37478c88cb6
dreamsbond <info@dreamsbond.com>
2020-03-04 12:25:31 +0800

    update.:

--- split/components.sh
+++ split/components.sh
@@ -5,4 +5,6 @@ BRANCHES='master develop'
 COMPONENTS=(
     'components/Uuid/:git@github.com:orzford/limoncello-uuid.git'
     'components/Ldap/:git@github.com:orzford/limoncello-ldap.git'
+    'components/Route/:git@github.com:orzford/limoncello-route.git'
+    'components/Validation/:git@github.com:orzford/limoncello-validation.git'
 )
